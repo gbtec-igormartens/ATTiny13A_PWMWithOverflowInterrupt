37835567ba1bd40a92f3d615ff8b28b242d8eb20
d3rbastl3r <igor.martens.83@gmail.com>
2018-08-14 17:35:47 +0200

    Comment the old code

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 #define MIN_ROTARY_VAL 22
 #define MAX_ROTARY_VAL 88
 
-volatile uint16_t pulseResetCounter = 0;
-volatile uint16_t setLow = MIN_ROTARY_VAL; // When the pwm signal should set to low
+//volatile uint16_t pulseResetCounter = 0; // Max value = 65535
+//volatile uint16_t setLow = MIN_ROTARY_VAL; // When the pwm signal should set to low
 
-uint16_t timer = 0;
+//uint16_t timer = 0;
 
-//Timer 0 overflow vector - this runs every time timer0 overflows
+// Timer 0 overflow vector - this runs every time timer0 overflows
 ISR(TIM0_OVF_vect) {
-	++pulseResetCounter;
+	//++pulseResetCounter;
 }
 
 void setup() {
@@ -32,24 +32,24 @@ int main(void) {
     setup();
 
     while (1) {
-		if (pulseResetCounter >= PULSE_RESET_VAL) {
-			pulseResetCounter = 0;
-			PORTB |= (1<<PB4);
-		}
+//		if (pulseResetCounter >= PULSE_RESET_VAL) {
+//			pulseResetCounter = 0;
+//			PORTB |= (1<<PB4);
+//		}
+//
+//		if (pulseResetCounter >= setOff) {
+//			PORTB &= ~(1<<PB4);
+//		}
+//
+//		++timer;
+//		if (timer == 20000) {
+//			timer = 0;
+//			++setLow;
+//		}
 
-		if (pulseResetCounter >= setOff) {
-			PORTB &= ~(1<<PB4);
-		}
-
-		++timer;
-		if (timer == 20000) {
-			timer = 0;
-			++setLow;
-		}
-
-		if (setLow >= MAX_ROTARY_VAL) {
-			setLow = MIN_ROTARY_VAL;
-		}
+//		if (setLow >= MAX_ROTARY_VAL) {
+//			setLow = MIN_ROTARY_VAL;
+//		}
     }
 
     return 0;
